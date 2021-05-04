079fa41a641779fa61719a7ac378107f35698951
Jane096 <jes7077@naver.com>
2021-05-04 22:12:44 +0900

    Add: 폰켓몬 풀이 추가

--- JE/1주차/PhoneKetMon.py
+++ JE/1주차/PhoneKetMon.py
@@ -1,1 +1,15 @@
-# remote push testing
+def solution(nums):
+    answer = 0
+
+    pick = len(nums) / 2
+    setNums = set(nums)
+    lengthSetNums = len(setNums)
+
+    if lengthSetNums > pick:
+        answer = pick
+        return answer
+
+    else:
+        answer = lengthSetNums
+
+    return answer
