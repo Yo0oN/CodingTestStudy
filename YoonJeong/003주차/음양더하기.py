e1fafdcc502f93089630526bee09f4ca68facdfe
YoonJeong <53729311+Yo0oN@users.noreply.github.com>
2021-05-18 00:26:43 +0900

    음양더하기 완료

---
+++ YoonJeong/003주차/음양더하기.py
@@ -0,0 +1,10 @@
+def solution(absolutes, signs):
+    answer = 0
+    
+    for absolute, sign in zip(absolutes, signs) :
+        if sign :
+            answer += absolute
+        else :
+            answer -= absolute
+    
+    return answer
