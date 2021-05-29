1595eb04d70dc0f08542028c4d418e2f61249d44
YoonJeong <53729311+Yo0oN@users.noreply.github.com>
2021-05-30 00:01:15 +0900

    기능개발 완료

---
+++ YoonJeong/004주차/기능개발.py
@@ -0,0 +1,27 @@
+def solution(progresses, speeds):
+    answer = []
+    flag = 0
+    
+    while True :
+        if not progresses :
+            answer.append(flag)
+            flag = 0
+            break
+            
+        q, r = divmod((100 - progresses[0]), speeds[0])
+        
+        if progresses[0] >= 100 :
+            flag += 1
+            progresses.pop(0)
+            speeds.pop(0)
+            continue
+        elif r == 0:
+            progresses = [i + (j * q) for i, j in zip(progresses, speeds)]
+        else :
+            progresses = [i + (j * (q + 1)) for i, j in zip(progresses, speeds)]
+            
+        if flag != 0 :
+            answer.append(flag)
+            flag = 0
+    
+    return answer
