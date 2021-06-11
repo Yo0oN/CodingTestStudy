e4ee4608808967d0d75b719b32d8950928895afb
YoonJeong <53729311+Yo0oN@users.noreply.github.com>
2021-06-12 04:07:30 +0900

    Update 예산.py

--- YoonJeong/006주차/예산.py
+++ YoonJeong/006주차/예산.py
@@ -1,3 +1,13 @@
+#while 이용
+def solution(d, budget):
+    answer = 0
+    d.sort()
+    while d and budget - d[0] >= 0 :
+        budget -= d.pop(0)
+        answer += 1
+    return answer
+
+#for 이용
 def solution(d, budget):
     answer = 0
     d.sort()
