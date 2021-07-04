63e9483b5c82ed2f2a71156104545b87ac22b994
juhee2 <rnlakro3@gmail.com>
2021-07-04 16:27:08 +0900

    [source] 2016년 (12901)

---
+++ juhee2/9주차/2016년.py
@@ -0,0 +1,16 @@
+# def solution(a, b):
+#     last_day = [31,29,31,30,31,30,31,31,30,31,30,31]
+#     day = ['FRI','SAT','SUN','MON','TUE','WED','THU']
+#     answer = ''
+#     days = 0
+#     for i in range(0,a-1):
+#         days += last_day[i]
+#     days += b
+#     days = days % 7
+#     return day[days - 1]
+
+def solution(a, b):
+    last_day = [31,29,31,30,31,30,31,31,30,31,30,31]
+    day = ['FRI','SAT','SUN','MON','TUE','WED','THU']
+    
+    return day[(sum([last_day[i] for i in range(0,a-1)]) + b) % 7 - 1]
