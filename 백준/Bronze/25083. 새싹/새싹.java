1c7a1a5494a69ea6c48124a3ef9a91b1ab1e0391
YoonJeong <53729311+Yo0oN@users.noreply.github.com>
2022-06-30 12:58:39 +0900

    [Bronze V] Title: 새싹, Time: 72 ms, Memory: 11328 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.println("         ,r'\"7");
+        System.out.println("r`-_   ,'  ,/");
+        System.out.println(" \\. \". L_r'");
+        System.out.println("   `~\\/");
+        System.out.println("      |");
+        System.out.println("      |");
+
+    }
+}
