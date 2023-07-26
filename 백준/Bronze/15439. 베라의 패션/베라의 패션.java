618c7edda85a594746a465ca2fb8139f207d3925
YoonJeong <53729311+Yo0oN@users.noreply.github.com>
2023-07-26 18:56:19 +0900

    [Bronze IV] Title: 베라의 패션, Time: 76 ms, Memory: 11532 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+public class Main {
+    private static BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    private static BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        int N = Integer.parseInt(bufferedReader.readLine());
+
+        int result = N * N - N;
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.flush();
+        bufferedWriter.close();
+    }
+}
